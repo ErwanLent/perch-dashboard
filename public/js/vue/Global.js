8b96e1f01b8498979bb4ae37decc744d0a499173
Erwan Lent <erwanlent@gmail.com>
2018-04-13 19:16:26 -0700

    Added 3d building data

--- public/js/vue/Global.js
+++ public/js/vue/Global.js
@@ -9,5 +9,4 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXJ3YW5sZW50IiwiYSI6IjA4YzY2Zjg2OTBkNDY5MDEyODBmN2RkOTdjMDc0NTY0In0.NY4En8vkN8h4JvlSDlhLfw';
    
-const LightMapStyleUrl = 'mapbox://styles/erwanlent/cj51l89ew1gi02rk86hbvwdv0';
-const SatelliteMapStyleUrl = 'mapbox://styles/erwanlent/cj51lj7kv1go22rs3ox6cmlrj';
+const LightMapStyleUrl = 'mapbox://styles/erwanlent/cjeao7w2u07kj2sqezqqf6s8u';
--- public/js/vue/components/MainMap.js
+++ public/js/vue/components/MainMap.js
@@ -1,11 +1,58 @@
 Vue.component('main-map', {
     template: '#main-map-template',
     data: function() {
-        return {};
+        return {
+            map: null
+        };
     },
     methods: {
         load: function() {
+            this.map = this.$refs.lightmap.map;
+            this.add3dBuildings();
+        },
+        add3dBuildings: function() {
+            const layers = this.map.getStyle().layers;
 
+            let labelLayerId;
+            for (let i = 0; i < layers.length; i++) {
+                if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+                    labelLayerId = layers[i].id;
+                    break;
+                }
+            }
+
+            this.map.addLayer({
+                'id': '3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'paint': {
+                    'fill-extrusion-color': '#aaa',
+
+                    'fill-extrusion-height': [
+                        "interpolate", ["linear"],
+                        ["zoom"],
+                        15, 0,
+                        15.05, ["get", "height"]
+                    ],
+                    'fill-extrusion-base': [
+                        "interpolate", ["linear"],
+                        ["zoom"],
+                        15, 0,
+                        15.05, ["get", "min_height"]
+                    ],
+                    'fill-extrusion-opacity': .6
+                }
+            }, labelLayerId);
+        },
+        zoomIn: function(lat, lon) {
+            this.map.flyTo({
+                center: [lon, lat],
+                zoom: 16,
+                pitch: 60
+            });
         }
     }
 });
