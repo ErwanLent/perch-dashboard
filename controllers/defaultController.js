cdbebf3037dde119b5dfeb3949d5ba9b7396b46b
Erwan Lent <erwanlent@gmail.com>
2018-04-14 18:48:18 -0700

    Added sub bar

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,5 +1,15 @@
-﻿const path = require('path');
+﻿const axios = require('axios');
+const path = require('path');
 
 exports.index = (req, res) => {
-    res.render(path.resolve('views/pages/index.ejs'));    
+    axios.get("http://flask-env.qfkjpmpp82.us-west-2.elasticbeanstalk.com/random_truck_schedule")
+        .then(function(response) {
+            res.render(path.resolve('views/pages/index.ejs'), {
+            	trucks: response.data.trucks
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+            res.send('An Error Occurred');
+        });
 };
